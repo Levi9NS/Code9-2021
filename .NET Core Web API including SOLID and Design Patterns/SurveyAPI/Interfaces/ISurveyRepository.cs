8cb75729d365ef028f37290df9b12601eb8c1da5
Marko Mitic <m.mitic@levi9.com>
2021-05-13 14:08:37 +0200

    Implementation of Survey API

---
+++ .NET Core Web API including SOLID and Design Patterns/SurveyAPI/Interfaces/ISurveyRepository.cs
@@ -0,0 +1,14 @@
+﻿using SurveyAPI.Models;
+
+namespace SurveyAPI.Interfaces
+{
+    public interface ISurveyRepository
+    {
+        Survey GetSurvey(int surveyId);
+        Survey AddSurvey(Survey survey); 
+        SurveyResult GetSurveyResult(int surveyId);
+        SurveyResult AddSurveyResult(SurveyResult survey);
+
+        void DeleteSurvey(int surveyId);
+    }
+}
---
+++ .NET Core Web API including SOLID and Design Patterns/SurveyAPI/Models/AnsweredQuestion.cs
@@ -0,0 +1,9 @@
+﻿namespace SurveyAPI.Models
+{
+    public class AnsweredQuestion
+    {
+        public string text { get; set; }
+        public string response { get; set; }
+        public int count { get; set; }
+    }
+}
